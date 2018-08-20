e0c22cbb8aaf0068e18be9de8c4046448adf78f2
OKAZAKI YUKO <yuko@okazakiyuukonoMacBook-Pro.local>
2018-08-20 09:25:26 +0800

    following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	before_action :require_login, only: [:index]
   def index
-  	@users = User.all
+  	@users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -71,6 +71,20 @@ class UsersController < ApplicationController
 			render "edit"
 		end
 
+	end
+
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.following
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers
+		render 'show_follow'
 	end
 
 	private
