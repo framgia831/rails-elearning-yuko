eeeb38d63c5da33b24ce6750e9018f23424a12b7
OKAZAKI YUKO <yuko@okazakiyuukonoMacBook-Pro.local>
2018-08-20 12:00:36 +0800

    dashboardPage

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :categories
   resources :lessons 
   resources :relationships
+  resources :dashboard
+  resources :words
 
   namespace :admin do
   	resources :categories do
